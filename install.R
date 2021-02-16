60ecb01a35370747e52d0b363bae06aaab01a9d4
Sam Lipworth <samuel.lipworth@ndm.ox.ac.uk>
2021-02-16 12:15:17 +0000

    Create install.R

---
+++ install.R
@@ -0,0 +1,18 @@
+install.packages('tidyverse')
+isntall.packages('ape')
+install.packages('igraph')
+install.packages('phytools')
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("ggtree")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("genbankr")
+install.packages('RColorBrewer')
+install.packages('ggnewscale')
+install.packages('gggenes')
+install.packages('grid')
+install.packages('gtable')
